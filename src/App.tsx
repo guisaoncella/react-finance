3f9bd361576ac5eac3c27e2bd401fa699ca98d00
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-09-22 14:45:15 -0300

    :sparkles: feat: filter list by month

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import { Body, Container, Header, HeaderText } from "./App.styles";
+import { Item } from "./types/Item";
+import { items } from "./data/items";
+import { filterListByMonth, getCurrentMonth } from "./util/dateFilter";
 
 const App = () => {
+    const [list, setList] = useState(items);
+    const [filteredList, setFilteredList] = useState<Item[]>([]);
+    const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+    useEffect(() => {
+        setFilteredList(filterListByMonth(list, currentMonth));
+    },[list, currentMonth])
+
     return(
         <Container>
             <Header>
