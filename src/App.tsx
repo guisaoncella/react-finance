3f6ab233d23e62db6e5262316eb310795aa8bb37
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-09-22 11:06:23 -0300

    :tada: init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./App.styles";
+
+const App = () => {
+    return(
+        <Container>
+            <h1>Hello</h1>
+        </Container>
+    );
+}
+
+export default App;
