bca375e8b12b30f3f14039c6a9649810ede0ea25
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-09-23 14:01:43 -0300

    :sparkles: feat: change month

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,16 @@ const App = () => {
     const [list, setList] = useState(items);
     const [filteredList, setFilteredList] = useState<Item[]>([]);
     const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+    const [income, setIncome] = useState(0);
+    const [expense, setExpense] = useState(0);
 
     useEffect(() => {
         setFilteredList(filterListByMonth(list, currentMonth));
     },[list, currentMonth])
+
+    const handleMonthChange = (newMonth: string) =>{
+        setCurrentMonth(newMonth);
+    }
 
     return(
         <Container>
@@ -21,7 +27,12 @@ const App = () => {
                 <HeaderText>Sistema Financeiro</HeaderText>
             </Header>
             <Body>
-                <InfoArea currentMonth={currentMonth}/>
+                <InfoArea 
+                    currentMonth={currentMonth}
+                    onMonthChange={handleMonthChange}
+                    income={income}
+                    expense={expense}
+                />
                 <TableArea list={filteredList}/>
             </Body>
         </Container>
--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -3,15 +3,25 @@ import { Container, MonthArea, MonthArrow, MonthTitle, ResumeArea } from "./styl
 
 type Props = {
     currentMonth: string;
+    onMonthChange: (newMonth: string) => void;
+    income: number;
+    expense: number;    
 }
 
-export const InfoArea = ({currentMonth}: Props) => {
+export const InfoArea = ({currentMonth, onMonthChange, income, expense}: Props) => {
+    const handleChangeMonth = (i: number) => {
+        let [year, month] = currentMonth.split('-');
+        let currentDate = new Date(parseInt(year), parseInt(month), 1);
+        currentDate.setMonth(currentDate.getMonth() + i);
+        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth()}}`);
+    }
+
     return (
         <Container>
             <MonthArea>
-                <MonthArrow>⬅️</MonthArrow>
+                <MonthArrow onClick={() => handleChangeMonth(-1)}>⬅️</MonthArrow>
                 <MonthTitle>{formatCurrentMonth(currentMonth)}</MonthTitle>
-                <MonthArrow>➡️</MonthArrow>
+                <MonthArrow onClick={() => handleChangeMonth(1)}>➡️</MonthArrow>
             </MonthArea>
             <ResumeArea>
 
